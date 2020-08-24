c6d1ea4bac3c859d96d95462533a95c988bbaa0a
Phương Anh Như <nhuphuonganh001@gmail.com>
2020-08-24 20:59:56 +0200

    check isbn10 v1

--- Program.cs
+++ Program.cs
@@ -6,7 +6,46 @@ namespace inbs10
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            // string code1 = "3-499-13599-X"; // Prüfziffer richtig
+            // string code2 = "3-44619313-8";   // Prüfziffer richtig
+            // string code3 = "0-7475-5100-6";  // Prüfziffer richtig
+            // string code4 = "1-57231-4222";  // Prüfziffer richtig
+            string code5 = "3 49913 599 X"; // Prüfziffer richtig
+            // string code6 = "1234-56789-0";
+
+            Console.WriteLine(ValidateIsbn10(code5));
+
+        }
+
+        static bool ValidateIsbn10(string code)
+        {
+
+            string normalizedCode = code.Replace("-", "").Replace(" ", "");
+            if (normalizedCode.Length != 10)
+            {
+                return false;
+            }
+            char[] codeArray = normalizedCode.ToCharArray();
+
+            int result = 0;
+
+            for (int i = 0; i < 9; i++)
+            {
+                result += (codeArray[i] - '0')*(i+1);
+            }
+
+            int z10 = result%11;
+
+            int checkNumber = 0;
+
+            if (codeArray[9] == 'X') {
+                checkNumber = 10;
+            } else {
+                checkNumber = codeArray[9] - '0';
+            }
+
+            return z10 == checkNumber;
         }
     }
 }
