ebd57b45a4eb4573449f8b9406ed67c04d57c6e5
Phương Anh Như <nhuphuonganh001@gmail.com>
2020-08-24 21:16:31 +0200

    code eingabe von console

--- Program.cs
+++ Program.cs
@@ -11,13 +11,19 @@ namespace inbs10
             // string code2 = "3-44619313-8";   // Prüfziffer richtig
             // string code3 = "0-7475-5100-6";  // Prüfziffer richtig
             // string code4 = "1-57231-4222";  // Prüfziffer richtig
-            string code5 = "3 49913 599 X"; // Prüfziffer richtig
-            // string code6 = "1234-56789-0";
+            //string code5 = "3 49913 599 X"; // Prüfziffer richtig
+                                            // string code6 = "1234-56789-0";
 
-            Console.WriteLine(ValidateIsbn10(code5));
+
+            PrintIsbn10Result();
 
         }
-
+        static void PrintIsbn10Result()
+        {
+            Console.WriteLine("Geben Sie isbn10 ein:");
+            string code = Console.ReadLine();
+            Console.WriteLine(ValidateIsbn10(code) ? "Prüfziffer richtig" : "Prüfziffer falsch");
+        }
         static bool ValidateIsbn10(string code)
         {
 
@@ -32,16 +38,19 @@ namespace inbs10
 
             for (int i = 0; i < 9; i++)
             {
-                result += (codeArray[i] - '0')*(i+1);
+                result += (codeArray[i] - '0') * (i + 1);
             }
 
-            int z10 = result%11;
+            int z10 = result % 11;
 
             int checkNumber = 0;
 
-            if (codeArray[9] == 'X') {
+            if (codeArray[9] == 'X')
+            {
                 checkNumber = 10;
-            } else {
+            }
+            else
+            {
                 checkNumber = codeArray[9] - '0';
             }
 
